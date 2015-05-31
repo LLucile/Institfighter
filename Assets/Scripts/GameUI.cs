141084d1273956e4c2ec9af158276208cda5264d
Loïc Huck <lojic@hotmail.fr>
2015-05-31 15:10:57 +0200

    Add UI

--- Assets/Scripts/GameUI.cs
+++ Assets/Scripts/GameUI.cs
@@ -1,15 +1,23 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 public class GameUI : MonoBehaviour {
 
+	public GameObject[] PlayerUI;
+	public Image[] playersHealth;
+
 	// Use this for initialization
 	void Start () {
-	
+
 	}
 	
 	// Update is called once per frame
 	void Update () {
 	
+	}
+
+	void SetHealth(int player, float amount){
+		playersHealth[player].fillAmount = Mathf.Abs(amount);
 	}
 }
